b07eea624a84c1f9838612c03e5b6009ebd28e91
dishahimani <disha.himani03@gmail.com>
2024-12-19 22:40:51 +0530

    Added Portfolio Website Files

---
+++ src/assets/index.js
@@ -0,0 +1,85 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import logo_dh  from "./logo_dh.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg"; 
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import  Intellimed_new from "./Intellimed_new.png";
+
+import intel from "./company/intel.png";
+import econhacksblr from "./company/econhacksblr.jpg";
+import neuraai from "./company/neuraai.jpeg";
+import bosch from "./company/bosch.jpeg";
+import apex from "./company/apex.jpeg";
+import gssoc from "./company/gssoc.png";
+import shefi from "./company/shefi.jpeg";
+import tsoc from "./company/tsoc.jpg";
+
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  intel,
+  econhacksblr,
+  neuraai,
+  bosch,
+  apex,
+  gssoc,
+  shefi,
+  tsoc,
+  Intellimed_new,
+  logo_dh,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,278 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  intel,
+  econhacksblr,
+  neuraai,
+  bosch,
+  apex,
+  gssoc,
+  shefi,
+  tsoc,
+  Intellimed_new,
+  logo_dh,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "AI - ML Developer",
+    icon: web,
+  },
+  {
+    title: "Web Developer",
+    icon: mobile,
+  },
+  {
+    title: "NLP and CV Developer",
+    icon: backend,
+  },
+  {
+    title: "Open Source Contributor",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "AI Apprentice",
+    company_name: "Intel, Niti Aayog",
+    icon: intel,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Mastered advanced Python and machine learning techniques in 3 major domains of AI: Computer Vision (CV), Natural Language Processing (NLP) and Data Projects.",
+      "Developed a high-accuracy machine learning canteen menu suggester for the AI4Youth x CBSE x Niti Aayog initiative. This project improved student nutrition and reduced food waste, making it a Top 15 national finalist.",
+      "Analyzed over 10,000 data points from data.gov.in to identify actionable insights aligned with 3 Sustainable Development Goals (SDGs). ",
+      
+    ],
+  },
+  {
+    title: "Logistics Manager",
+    company_name: "EconHacks Bangalore",
+    icon: econhacksblr,
+    iconBg: "#E6DEDD",
+    date: "April 2021 - Oct 2021",
+    points: [
+      "Organized the largest youth-led economics hackathon for high school ",
+      "It was a 48 hour virtual event held during the COVID-19 pandemic.",
+      "The main aim was to tackle global economic issues through innovative tech solutions",
+    ],
+  },
+  {
+    title: "AI Strategy Analyst",
+    company_name: "Neura-AI",
+    icon: neuraai,
+    iconBg: "#383E56",
+    date: "Oct 2022 - Sept 2023",
+    points: [
+      "Led and actively participated in AI & Machine Learning workshops and events for the student club",
+      "Organized the college's largest club event, a 2-day Industry Conclave featuring over 6 AI, ML, Cybersecurity professionals, facilitating knowledge exchange between industry experts and students.",
+    ],
+  },
+  {
+    title: "Intern",
+    company_name: "Bosch Global Software Technologies Private Limited.",
+    icon: bosch,
+    iconBg: "#E6DEDD",
+    date: "Jun 2023 - Aug 2023",
+    points: [
+      "Implemented a real-time allocation system that improved Electric Vehicle (EV) allocation to charging stations in major cities. ",
+      "Compared and implemented multiple optimization algorithms (Dijkstra, Ant Colony, etc.) to improve the efficiency of assigning EVs to charging stations.",
+      "Evaluated a dataset of 150+ charging stations across metropolitan areas.",
+    ],
+  },
+  {
+    title: "Vice President",
+    company_name: "APEX",
+    icon: apex,
+    iconBg: "#383E56",
+    date: "Sept 2023 - Feb 2024",
+    points: [
+      "Organized workshops and events to gain community engagement.",
+      "Mentored various students and guided them through their coursework, academics and projects.",
+    ],
+  },
+  {
+    title: "Open Source Contributor",
+    company_name: "GirlScript Summer of Code (GSSOC)",
+    icon: gssoc,
+    iconBg: "#E6DEDD",
+    date: "May 2024 - Aug 2024",
+    points: [
+      "A 3 month long program that focuses on contributing to open source projects in the domains of Artificial Intelligence, Machine Learning, and Web Development. ",
+      "Working alongside a global community of developers. Engaging in code reviews, discussions, and collaborative problem-solving to enhance project functionality and performance.",
+      "A hands-on experience with Git, GitHub, and open source contribution.",
+    ],
+  },
+  {
+    title: "SheFi Scholar",
+    company_name: "SheFi",
+    icon: shefi,
+    iconBg: "#E6DEDD",
+    date: "Jun 2023 - Present",
+    points: [
+      "Joined Shefi, a vibrant women-centric community fostering learning and engagement in Web3 and blockchain technologies.",
+      "Actively engaging and exploring topics such as cryptocurrencies, NFTs (Non-Fungible Tokens), Decentralized Finance (DeFi), Ethereum, and trading tokens.",
+    ],
+  },
+  {
+    title: "Contributor",
+    company_name: "TimeChain Summer of Code (TSOC)",
+    icon: tsoc,
+    iconBg: "#E6DEDD",
+    date: "Jun 2023 - Present",
+    points: [
+      "A global online summer internship program focusing on Bitcoin SV (BSV) open-source development and design.",
+      "Opportunity to gain practical experience in Bitcoin SV open-source project development and design with industry mentors and a global community.",
+    ],
+  },
+
+
+
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "IntelliMed",
+    description:
+      "AI Virtual Doctor Assistant that diagnoses potential illnesses based on symptoms. Trained on a custom dataset of 32 illnesses and 140+ symptoms, it offers multilingual support (10 languages) and provides guidance on remedies, doctor visits, and dietary adjustments.",
+    tags: [
+      {
+        name: "AI",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "ML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Python",
+        color: "pink-text-gradient",
+      },
+    ],
+    image:  Intellimed_new,
+    source_code_link: "https://github.com/dishahimani/IntelliMed",
+  },
+  
+];
+
+export { services, technologies, experiences, testimonials, projects };
